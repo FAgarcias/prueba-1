12cbbc3fcecebbace2fe0a5891c1e56a663b5396
fagarcias <garciafergie997@gmail.com>
2024-07-18 15:56:32 -0500

    cambio6

---
+++ miprimer.js
@@ -0,0 +1,53 @@
+/*
+for (var i = 0; i<=5; i++){
+    if(i%2 === 0){
+        console.log(i)
+    }
+}
+*/
+
+/*
+function foo(objeto) {
+    return objeto['clave'] + objeto['adicional']
+} 
+
+var my_var = {
+    clave: 'valor ',
+    otro: 'dato',
+    adicional: 'animal'
+};
+
+var resultado = foo(my_var)
+
+console.log(resultado)
+
+ */
+/*
+var myvar = 1;
+myvar = myvar++ + 1;
+console.log(myvar)
+*/
+/*
+var x;
+var resultado = typeof (x);
+console.log(resultado)
+*/
+/*
+var resultado = 1 + 2 - 3 * 4 / 5;
+console.log(resultado)
+*/
+/*
+function foo(s) {
+    return s.length;
+}
+    */
+
+var resultado;
+if (4 == 4){
+    resultado = 'Resultado A';
+} else{
+    resultado = 'Resultado B';
+}
+console.log(resultado)
+
+
